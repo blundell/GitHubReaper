5503cf2269f11552ea7dd84b75b06d38b4520d32
Blundell <paul@novoda.com>
2019-04-26 14:46:01 +0100

    don't use internal apis

--- src/main/java/com/blundell/github/reaper/Credentials.java
+++ src/main/java/com/blundell/github/reaper/Credentials.java
@@ -1,13 +1,13 @@
 package com.blundell.github.reaper;
 
-import static com.squareup.okhttp.internal.Util.UTF_8;
+import java.nio.charset.Charset;
 
 public class Credentials {
     private final String username;
     private final String hashedCredentials;
 
     public static Credentials generate(String username, String password) {
-        String hashedCredentials = new String(Base64Coder.encode((username + ":" + password).getBytes(UTF_8)));
+        String hashedCredentials = new String(Base64Coder.encode((username + ":" + password).getBytes(Charset.forName("UTF-8"))));
         return new Credentials(username, hashedCredentials);
     }
 
